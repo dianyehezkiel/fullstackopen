3a82d3e10c2fbe7c7f3c843af33089c7ff88772e
dianyehezkiel <dianyehezkiel@gmail.com>
2022-03-23 15:28:40 +0700

    done exercise 3.2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,15 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const date = new Date()
+
+  const info = `<p>Phonebook has info for ${persons.length} people</p>
+  <p>${date}</p>`
+
+  response.send(info)
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
