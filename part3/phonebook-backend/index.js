df930455c6a45b4e54231dbc897ffb7857d645ae
dianyehezkiel <dianyehezkiel@gmail.com>
2022-03-23 17:05:32 +0700

    done exercise 3.7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,10 @@
 const express =  require('express')
 const app = express()
+const morgan = require('morgan')
+const morganTiny = morgan('tiny')
+
 app.use(express.json())
+app.use(morganTiny)
 
 persons = [
   { 
