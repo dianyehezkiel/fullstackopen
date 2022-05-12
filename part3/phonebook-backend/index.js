6d39010d5f90395a70351f294151d1fe404f8e8f
dianyehezkiel <dianyehezkiel@gmail.com>
2022-05-12 14:28:13 +0700

    done exercise 3.17

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -64,6 +64,21 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(res => {
