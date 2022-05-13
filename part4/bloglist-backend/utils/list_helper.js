20877e8ba75d6d3e9439f7ffd140d885d11a3b7c
dianyehezkiel <dianyehezkiel@gmail.com>
2022-05-14 00:41:26 +0700

    done exercise 4.4

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -1,6 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+  const reducer = (prev, curr) => prev + curr.likes;
+
+  return blogs.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
