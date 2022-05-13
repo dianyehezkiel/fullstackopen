d9322fbf3f46fa0749de8cd5a3c9d59bf6228413
dianyehezkiel <dianyehezkiel@gmail.com>
2022-05-13 12:06:15 +0700

    update app.js

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -1,5 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
 const app = express();
+
+logger.info('Connecting to', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
 
 module.exports = app;
