a7dcde79abaf43d48cd0b9e504a68d3d3ee3e443
dianyehezkiel <dianyehezkiel@gmail.com>
2022-05-16 12:11:37 +0700

    done exercise 4.8

---
+++ part4/bloglist-backend/test/bloglist_api.test.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+const testHelper = require('./test_helper');
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const blogObjects = testHelper.initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs');
+
+  expect(response.body).toHaveLength(testHelper.initialBlogs.length);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
