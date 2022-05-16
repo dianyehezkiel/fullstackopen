a6e4b811f15298bcd904bdb89885f0d7ad65ce9c
dianyehezkiel <dianyehezkiel@gmail.com>
2022-05-16 12:04:17 +0700

    configure test

--- part4/bloglist-backend/utils/config.js
+++ part4/bloglist-backend/utils/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
-const { MONGODB_URI } = process.env;
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI;
 const { PORT } = process.env;
 
 module.exports = {
