94b4b8865dad409d01973487e0be4eee42cb15a4
dianyehezkiel <dianyehezkiel@gmail.com>
2022-05-16 13:17:43 +0700

    done exercise 4.11

--- part4/bloglist-backend/test/bloglist_api.test.js
+++ part4/bloglist-backend/test/bloglist_api.test.js
@@ -48,6 +48,25 @@ test('a valid blog can be added', async () => {
   expect(titles).toContain('Go To Statement Considered Harmful');
 });
 
+test('likes property default to zero if missing', async () => {
+  const newBlog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const allBlogs = await testHelper.blogsInDb();
+  const lastBlog = allBlogs[allBlogs.length - 1];
+
+  expect(lastBlog.likes).toBe(0);
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
