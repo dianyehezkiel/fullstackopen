e9c9e4dc8e4827c4f0b81c1270445c104179382d
dianyehezkiel <dianyehezkiel@gmail.com>
2022-05-16 12:10:16 +0700

    create testHelper to store initial blogs data

---
+++ part4/bloglist-backend/test/test_helper.js
@@ -0,0 +1,24 @@
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+  },
+  {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+  },
+];
+
+module.exports = {
+  initialBlogs,
+};
