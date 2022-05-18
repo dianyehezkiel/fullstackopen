38f9faa331fd09d3e9b2314230d5cc092fa3da80
dianyehezkiel <dianyehezkiel@gmail.com>
2022-05-19 00:28:02 +0700

    done exercise 4.18

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('express-async-errors');
+const loginRouter = require('./controllers/login');
 const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
 const config = require('./utils/config');
@@ -24,6 +25,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
