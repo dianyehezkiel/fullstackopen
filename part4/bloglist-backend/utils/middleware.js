1cc5d14016a270ea3666dee7263e026e5c8b01cf
dianyehezkiel <dianyehezkiel@gmail.com>
2022-05-19 22:30:54 +0700

    update error handler

--- part4/bloglist-backend/utils/middleware.js
+++ part4/bloglist-backend/utils/middleware.js
@@ -27,6 +27,11 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
   }
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token',
+    });
+  }
 
   next(error);
 };
