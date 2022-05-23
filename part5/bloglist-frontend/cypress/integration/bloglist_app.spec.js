c911467b70f193d2c448c5cddb937d616e1c999d
dianyehezkiel <dianyehezkiel@gmail.com>
2022-05-23 23:50:21 +0700

    done exercise 5.18

--- part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
@@ -7,6 +7,13 @@
 describe('Blog App', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const newUser = {
+      username: 'dianyehezkiel',
+      name: 'Dian Yehezkiel',
+      password: 'pa55word'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', newUser)
+
     cy.visit('http://localhost:3000')
   })
 
@@ -17,5 +24,26 @@ describe('Blog App', function() {
     cy.get('#username-input').should('be.visible')
     cy.get('#password-input').should('be.visible')
     cy.get('#login-button').contains('Login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username-input').type('dianyehezkiel', { delay: 100 })
+      cy.get('#password-input').type('pa55word', { delay: 100 })
+      cy.get('#login-button').click()
+
+      cy.contains('Welcome back, Dian Yehezkiel')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username-input').type('dianyehezkiel', { delay: 100 })
+      cy.get('#password-input').type('wrong', { delay: 100 })
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'Incorrect Username or Password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+    })
   })
 })
