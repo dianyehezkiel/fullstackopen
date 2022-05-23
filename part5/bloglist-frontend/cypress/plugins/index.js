efdf92a36f718702342a3de6cde57c48cddc6857
dianyehezkiel <dianyehezkiel@gmail.com>
2022-05-24 01:37:18 +0700

    fix lint problem

--- part5/bloglist-frontend/cypress/plugins/index.js
+++ part5/bloglist-frontend/cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
