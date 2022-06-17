db2afe308ad8299f7d85b5e52f7f9ce59de60625
dianyehezkiel <dianyehezkiel@gmail.com>
2022-06-17 23:59:40 +0700

    update Users component style

--- part7/bloglist-frontend/src/components/Users.js
+++ part7/bloglist-frontend/src/components/Users.js
@@ -1,6 +1,17 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import {
+  Box,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  Paper,
+  TableBody,
+  TableCell,
+} from '@mui/material'
 import { initUsers } from '../reducers/usersReducer'
 
 const Users = () => {
@@ -13,27 +24,50 @@ const Users = () => {
   const users = useSelector(({ users }) => users)
 
   return (
-    <div>
-      <h3>users</h3>
-      <table style={{ border: 'none' }}>
-        <thead>
-          <tr>
-            <th></th>
-            <th>blogs created</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>
-                <Link to={`/users/${user.id}`}>{user.name}</Link>
-              </td>
-              <td>{user.blogs.length}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Box>
+      <Typography component="h3" variant="h6">
+        Users
+      </Typography>
+      <TableContainer
+        sx={{
+          width: {
+            xs: '100%',
+            md: '50%',
+          },
+        }}
+        component={Paper}
+      >
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="center">Blogs Created</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow
+                hover
+                key={user.id}
+                component={Link}
+                to={`/users/${user.id}`}
+              >
+                <TableCell>
+                  <Typography component="p" variant="body2">
+                    {user.name}
+                  </Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography component="p" variant="body2">
+                    {user.blogs.length}
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   )
 }
 
