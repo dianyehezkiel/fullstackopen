b09f27a0e72c1418cd7f224e764eac851fbae088
dianyehezkiel <dianyehezkiel@gmail.com>
2022-06-17 22:55:01 +0700

    update Comment component style

--- part7/bloglist-frontend/src/components/Comments.js
+++ part7/bloglist-frontend/src/components/Comments.js
@@ -1,5 +1,16 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import {
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  TextField,
+  Typography,
+} from '@mui/material'
+import CommentIcon from '@mui/icons-material/Comment'
 import { addComment } from '../reducers/blogsReducer'
 
 const Comments = ({ id, comments }) => {
@@ -10,31 +21,75 @@ const Comments = ({ id, comments }) => {
   const handleComment = (e) => {
     e.preventDefault()
     dispatch(addComment(id, { comment: newComment }))
+    setNewComment('')
   }
 
   return (
-    <>
-      <h4>comments</h4>
-      <form onSubmit={handleComment}>
-        <input
+    <Box>
+      <Typography
+        sx={{
+          mt: '16px',
+          mb: '8px',
+          fontWeight: 'bold',
+        }}
+        component="h4"
+        variant="body1"
+      >
+        Comments
+      </Typography>
+      <Box
+        sx={{
+          width: {
+            xs: '100%',
+            md: '50%',
+          },
+        }}
+        component="form"
+        onSubmit={handleComment}
+      >
+        <TextField
+          sx={{
+            width: '100%',
+          }}
+          hiddenLabel
+          multiline
+          rows={2}
+          variant="filled"
           name="inputComment"
           value={newComment}
           onChange={({ target }) => setNewComment(target.value)}
         />
-        <button type="submit">add comment</button>
-      </form>
+        <Button
+          sx={{
+            display: 'block',
+            ml: 'auto',
+            mr: '0',
+            mt: '4px',
+          }}
+          color="success"
+          variant="contained"
+          type="submit"
+        >
+          Sent
+        </Button>
+      </Box>
       {comments.length === 0 ? (
-        <p>
-          <i>no comment yet</i>
-        </p>
+        <Typography component="i" variant="body2">
+          Be the first to comment!!
+        </Typography>
       ) : (
-        <ul>
+        <List>
           {comments.map((comment, index) => (
-            <li key={id + index}>{comment}</li>
+            <ListItem key={id + index} sx={{ alignItems: 'flex-start' }}>
+              <ListItemIcon>
+                <CommentIcon />
+              </ListItemIcon>
+              <ListItemText sx={{ mt: '0' }}>{comment}</ListItemText>
+            </ListItem>
           ))}
-        </ul>
+        </List>
       )}
-    </>
+    </Box>
   )
 }
 
