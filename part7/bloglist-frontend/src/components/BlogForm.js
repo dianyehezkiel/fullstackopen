d92259c41a7bd5b88357181592ba1da1c8c15f54
dianyehezkiel <dianyehezkiel@gmail.com>
2022-06-18 00:38:27 +0700

    update LoginForm Component style

--- part7/bloglist-frontend/src/components/BlogForm.js
+++ part7/bloglist-frontend/src/components/BlogForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import PropTypes from 'prop-types'
-import { FormGroup, Typography, TextField, Button } from '@mui/material'
+import { Box, FormGroup, Typography, TextField, Button } from '@mui/material'
 
 const BlogForm = ({ createBlog }) => {
   const [title, setTitle] = useState('')
@@ -21,7 +21,7 @@ const BlogForm = ({ createBlog }) => {
   }
 
   return (
-    <>
+    <Box sx={{ width: { xs: '100%', md: '50%' } }}>
       <Typography
         sx={{
           mb: '8px',
@@ -31,38 +31,40 @@ const BlogForm = ({ createBlog }) => {
       >
         Create New
       </Typography>
-      <FormGroup id="blog-form" sx={{ width: { xs: '100%', md: '50%' } }}>
-        <TextField
-          id="title-input"
-          label="Title"
-          variant="standard"
-          value={title}
-          onChange={({ target }) => setTitle(target.value)}
-        />
-        <TextField
-          id="author-input"
-          label="Author"
-          variant="standard"
-          value={author}
-          onChange={({ target }) => setAuthor(target.value)}
-        />
-        <TextField
-          id="url-input"
-          label="URL"
-          variant="standard"
-          value={url}
-          onChange={({ target }) => setUrl(target.value)}
-        />
-        <Button
-          sx={{ mt: '8px' }}
-          id="create-button"
-          variant="contained"
-          onClick={addBlog}
-        >
-          Create
-        </Button>
-      </FormGroup>
-    </>
+      <form id="blog-form" onSubmit={addBlog}>
+        <FormGroup>
+          <TextField
+            id="title-input"
+            label="Title"
+            variant="standard"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+          <TextField
+            id="author-input"
+            label="Author"
+            variant="standard"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+          <TextField
+            id="url-input"
+            label="URL"
+            variant="standard"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+          />
+          <Button
+            sx={{ mt: '8px' }}
+            id="create-button"
+            variant="contained"
+            type='submit'
+          >
+            Create
+          </Button>
+        </FormGroup>
+      </form>
+    </Box>
   )
 }
 
