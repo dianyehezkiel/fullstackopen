33858799932271e77eb0df9b52d8209c85f4944b
dianyehezkiel <dianyehezkiel@gmail.com>
2022-07-08 18:41:02 +0700

    done exercise 8.4

--- part8/library/index.js
+++ part8/library/index.js
@@ -107,7 +107,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -116,7 +116,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if(!args.author) {
+        return books
+      }
+
+      return books.filter((b) => b.author === args.author)
+    },
     allAuthors: () => authors.map((a) => {
       const bookCount = books.reduce((n, b) => {
         if(b.author === a.name) {
