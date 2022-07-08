7ea500dc10a93172673c3ed480611d1cb80d782d
dianyehezkiel <dianyehezkiel@gmail.com>
2022-07-08 18:09:15 +0700

    done exercise 8.1

--- part8/library/index.js
+++ part8/library/index.js
@@ -90,11 +90,15 @@ let books = [
 
 const typeDefs = gql`
   type Query {
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   }
 }
 
