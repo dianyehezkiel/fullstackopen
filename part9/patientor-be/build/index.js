e2d7592aecda1de5078d65aaf35caa1dea1dd09e
dianyehezkiel <dianyehezkiel@gmail.com>
2022-07-19 10:48:56 +0700

    done exercise 9.9

--- part9/patientor-be/build/index.js
+++ part9/patientor-be/build/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const PORT = 3000;
+app.use((0, cors_1.default)());
+const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
     res.send('hello, patientor!');
 });
